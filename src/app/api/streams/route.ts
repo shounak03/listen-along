654efc0b132fe603e7336204237723019a7ff3ca
shounak03 <shounakmis.3@gmail.com>
2024-09-27 13:43:57 +0530

    all the api routes completed

--- src/app/api/streams/route.ts
+++ src/app/api/streams/route.ts
@@ -1,36 +1,46 @@
 import { prismaClient } from '@/lib/db';
+import { YT_REGEX } from '@/lib/utils';
 import { NextRequest, NextResponse } from 'next/server';
-import {z} from "zod"
-const YT_REGEX = new RegExp("https:\/\/(www\.)?youtube\.com\/watch\?v=[\w-]+(&t=\d+s)?")
+import { z } from "zod"
+//@ts-ignore
+import youtubesearchapi from 'youtube-search-api'
 
 const CreateStreamSchema = z.object({
     creatorId: z.string(),
     url: z.string()
 })
 
-export async function  POST(req:NextRequest) {
+export async function POST(req: NextRequest) {
     try {
         const data = CreateStreamSchema.parse(await req.json());
-        const isYt = YT_REGEX.test(data.url)
-        if(!isYt){
+        const isYt = data.url.match(YT_REGEX)
+        if (!isYt) {
 
             return NextResponse.json({
                 message: "Unknown url"
-            },{
-                status:411
+            }, {
+                status: 411
             })
         }
         const extractedId = data.url.split("?v=")[1]
 
+        const res = await youtubesearchapi.GetVideoDetails(extractedId)
+        console.log(res);
         
-        const stream =  await prismaClient.stream.create({
-            data:{
-                
-                userId:data.creatorId,
+        const Thumbnails = res.thumbnail.thumbnails
+        Thumbnails.sort((a: { width: number }, b: { width: number }) => a.width < b.width ? -1 : 1)
+
+        const stream = await prismaClient.stream.create({
+            data: {
+
+                userId: data.creatorId,
                 url: data.url,
                 extractedId,
-                type:"Youtube"
-            
+                type: "Youtube",
+                title: res.title ?? "Cant find video",
+                smallImg: (Thumbnails > 1 ? Thumbnails[Thumbnails.length - 2].url : Thumbnails[Thumbnails.length - 1].url) ?? "https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=",
+                bigImg: Thumbnails[Thumbnails.length - 1].url ?? "https://media.istockphoto.com/id/1175435360/vector/music-note-icon-vector-illustration.jpg?s=612x612&w=0&k=20&c=R7s6RR849L57bv_c7jMIFRW4H87-FjLB8sqZ08mN0OU=",
+
             }
         });
 
@@ -40,18 +50,20 @@ export async function  POST(req:NextRequest) {
 
         })
     } catch (e) {
+        console.log(e);
+        
         return NextResponse.json({
             message: "Error while adding a stream"
-        },{
-            status:411
+        }, {
+            status: 411
         })
     }
 }
 
-export async function  GET(req:NextRequest) {
+export async function GET(req: NextRequest) {
     const creatorId = req.nextUrl.searchParams.get("creatorId");
     const streams = await prismaClient.stream.findMany({
-        where:{
+        where: {
             userId: creatorId ?? ""
 
         }
